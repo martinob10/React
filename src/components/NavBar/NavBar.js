7f6230c544e81d5906054db211af66972d982255
Martin F. Lucero <martin.f.lucero@gmail.com>
2022-07-21 01:19:29 -0300

    subiendo otra vez navbar bootstrap

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+// UpperCamelCase
+
+const NavBar = () => {
+    return(
+        <div>
+            <nav className="navbar bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="index.html">Navbar</a>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
