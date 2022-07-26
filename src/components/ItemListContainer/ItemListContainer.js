57992b57ad2ebbd4acba4b2846ee9fc58dd18f15
Martin F. Lucero <martin.f.lucero@hotmail.com>
2022-07-26 00:45:24 -0300

    contador

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,23 @@
 import ItemProduct from "../ItemProduct/ItemProduct"
+import './ItemListContainer.css'
 
 const ItemListContainer = ({section}) => {
+    //productos
+    const producto1 = {
+        title: "Cerradura Americana",
+        price: 3500,
+        image: 'cerradura0.jpg'
+    }
+    const producto2 = {
+        title: "Cerradura Americana",
+        price: 4500,
+        image: 'cerradura2.jfif'
+    }
     return(
         <div className="lista-productos">
             <h3>{section}</h3>
-            <ItemProduct title="Cerradura Americana" price="$3.500" image={'cerradura0.jpg'}/>
-            <ItemProduct title="Cerradura Italiana" price="$4.500" image={'cerradura2.jfif'}/>
+            <ItemProduct data={producto1}/>
+            <ItemProduct data={producto2}/>
             
         </div>
     )
--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,9 +1,31 @@
-const ItemProduct = ({title, price, image}) => {
+import { useState } from 'react'
+import './ItemProduct.css'
+
+const ItemProduct = ({data}) => {
+    //contador
+    const [contador, setContador] = useState(1)
+
+    const {title, image, price} = data
+
+    const addNumber = () =>{
+        setContador(contador + 1)
+    }
+
+    const removeNumber = () =>{
+        setContador(contador - 1)
+    }
+
+
     return(
-        <div>
+        <div className="ItemProduct">
             <img src={`/assets/${image}`} alt="Cerradura" />
             <p>{title}</p>
-            <span>{price}</span>
+            <span>$ {price}</span>
+            <div className="ContadorProducto">
+                <button onClick={removeNumber}> - </button>
+                <p>{contador}</p>
+                <button onClick={addNumber}> + </button>
+            </div>
             <button>Comprar</button>
         </div>
     )
